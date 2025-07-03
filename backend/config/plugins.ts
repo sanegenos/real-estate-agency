e98a416d2726ba49cda2f56965395abaaf5d7914
sanegenos <izhaevm@gmail.com>
2025-07-03 18:44:25 +0600

    Fix img problems 3

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -5,18 +5,19 @@ export default ({ env }) => ({
     config: {
       jwtSecret: env('JWT_SECRET') || env('ADMIN_JWT_SECRET'),
     },
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key:    env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload:   {},
-        delete:   {},
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key:    env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload:   {},
+          delete:   {},
+        },
       },
     },
-  },
+  }
 });
