50c2af4f925233ee908bbf8e7dd2d62fe8f44269
sanegenos <izhaevm@gmail.com>
2025-07-09 20:16:38 +0600

    Fix img problems 12

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,22 +1,25 @@
-// backend/config/plugins.ts
+// src/backend/config/plugins.ts
 export default ({ env }) => ({
+  // Конфиг плагина users-permissions оставляем
   'users-permissions': {
     config: {
       jwtSecret: env('JWT_SECRET') || env('ADMIN_JWT_SECRET'),
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key:    env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload:   { folder: 'real-estate' },
-          delete:   {},
-        },
+  },
+
+  // Вынесенный на верхний уровень конфиг upload
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key:    env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: { folder: 'real-estate' },
+        delete: {},
       },
     },
-  }
+  },
 });
