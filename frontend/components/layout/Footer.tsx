3e444ee8fd5ef490015b4c3335f2a90b9b4bfb08
sanegenos <izhaevm@gmail.com>
2025-07-11 20:23:10 +0600

    footer, header, home edits

--- frontend/components/layout/Footer.tsx
+++ frontend/components/layout/Footer.tsx
@@ -70,7 +70,7 @@ export default function Footer() {
           <div className='flex flex-col items-center md:items-start'>
             <h3 className="font-bold text-gray-900 mb-4 text-center text-xl">Sılpağar Grup Ofisi</h3>
             <ul className="space-y-2">
-              <li className="flex items-start text-gray-600 hover:text-[#eecd32]">
+              <li className="flex items-start text-gray-600 hover:text-[#eecd32] transition-colors">
                 <svg className="w-8 h-8 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -79,7 +79,7 @@ export default function Footer() {
                   Beyoğlu/İstanbul
                 </a>
               </li>
-              <li className="flex items-start text-gray-600 hover:text-[#eecd32]">
+              <li className="flex items-start text-gray-600 hover:text-[#eecd32] transition-colors">
                 <svg className="w-8 h-8 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -94,13 +94,13 @@ export default function Footer() {
           <div className='flex flex-col items-center md:items-start'>
             <h3 className="font-bold text-gray-900 mb-4 text-center text-xl">İletişim</h3>
             <ul className="space-y-2">
-              <li className="flex items-center text-gray-600 hover:text-[#eecd32]">
+              <li className="flex items-center text-gray-600 hover:text-[#eecd32] transition-colors">
                 <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
                 <a href="tel:+905057050799" target='_blank'>+90 505 705 07 99</a>
               </li>
-              <li className="flex items-center text-gray-600 hover:text-[#eecd32]">
+              <li className="flex items-center text-gray-600 hover:text-[#eecd32] transition-colors">
                 <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
